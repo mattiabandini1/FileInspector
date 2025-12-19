8222cd921d75b033e3e9a16ee509a88d2dd94f8d
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2025-12-19 18:30:34 +0100

    feat: icon and text colored

--- file_inspector.sh
+++ file_inspector.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+R="\e[31m"
+G="\e[32m"
+RES="\e[0m"
+ICON_OK="${G}[✔]${RES}"
+ICON_KO="${R}[✖]${RES}"
+
 if (( $# != 1 )); then
     echo "Error, you need to pass a file as an argument"
     exit 1
@@ -17,27 +23,27 @@ fi
 clear
 
 if [[ -r ${FILE} ]]; then
-    R_ICON="[x]"
-    R_TEXT="YES"
+    R_ICON="${ICON_OK}"
+    R_TEXT="${G}YES${RES}"
 else
-    R_ICON="[ ]"
-    R_TEXT="NO"
+    R_ICON="${ICON_KO}"
+    R_TEXT="${R}NO${RES}"
 fi
 
 if [[ -w ${FILE} ]]; then
-    W_ICON="[x]"
-    W_TEXT="YES"
+    W_ICON="${ICON_OK}"
+    W_TEXT="${G}YES${RES}"
 else
-    W_ICON="[ ]"
-    W_TEXT="NO"
+    W_ICON="${ICON_KO}"
+    W_TEXT="${R}NO${RES}"
 fi
 
 if [[ -x ${FILE} ]]; then
-    X_ICON="[x]"
-    X_TEXT="YES"
+    X_ICON="${ICON_OK}"
+    X_TEXT="${G}YES${RES}"
 else
-    X_ICON="[ ]"
-    X_TEXT="NO"
+    X_ICON="${ICON_KO}"
+    X_TEXT="${R}NO${RES}"
 fi
 
 # UI
@@ -58,9 +64,9 @@ echo "-----------"
 printf "%-12s %-5s %s\n" "Type" "Check" "Status"
 echo "------------ ----- ------"
 
-printf "%-12s %-5s %s\n" "Read"    "${R_ICON}" "${R_TEXT}"
-printf "%-12s %-5s %s\n" "Write"   "${W_ICON}" "${W_TEXT}"
-printf "%-12s %-5s %s\n" "Execute" "${X_ICON}" "${X_TEXT}"
+printf "%-12s %-5b %b\n" "Read"    "${R_ICON}" "${R_TEXT}"
+printf "%-12s %-5b %b\n" "Write"   "${W_ICON}" "${W_TEXT}"
+printf "%-12s %-5b %b\n" "Execute" "${X_ICON}" "${X_TEXT}"
 
 echo ""
 echo "========================================"
