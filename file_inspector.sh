426f068d18b9252ee4c23736bb3b83ac24d7a2f6
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2025-12-23 06:08:05 +0100

    version: 0.2.0 complete, new UI implemented

--- file_inspector.sh
+++ file_inspector.sh
@@ -2,16 +2,35 @@
 
 R="\e[31m"
 G="\e[32m"
+C="\e[36m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+W="\e[97m"
+
 RES="\e[0m"
 
 BOLD="\e[1m"
 DIM="\e[2m"
+U="\e[4m"
+
+COLOR_BRAND="${C}"
+COLOR_ACCENT="${C}${BOLD}"
+COLOR_DIM="${DIM}${C}"
+COLOR_SUCCESS="${G}"
+COLOR_ERROR="${R}"
 
 ICON_OK="${G}[✔]${RES}"
 ICON_KO="${R}[✖]${RES}"
 
 if (( $# != 1 )); then
     echo "Error, you need to pass a file as an argument"
+    exit 1
+fi
+
+if (( $# != 1 )); then
+    echo -e "${COLOR_ERROR}${BOLD}Error:${RESET} You need specify a target file."
+    echo -e "${COLOR_BRAND}Use:${RESET} $0 <filename>"
     exit 1
 fi
 
@@ -27,50 +46,58 @@ fi
 clear
 
 if [[ -r ${FILE} ]]; then
-    R_ICON="${ICON_OK}"
-    R_TEXT="${G}YES${RES}"
+    R_CHECK="${ICON_OK}"
+    R_TEXT="${COLOR_SUCCESS}YES${RES}"
 else
-    R_ICON="${ICON_KO}"
-    R_TEXT="${R}NO${RES}"
+    R_CHECK="${ICON_KO}"
+    R_TEXT="${COLOR_ERROR}NO${RES}"
 fi
 
 if [[ -w ${FILE} ]]; then
-    W_ICON="${ICON_OK}"
-    W_TEXT="${G}YES${RES}"
+    W_CHECK="${ICON_OK}"
+    W_TEXT="${COLOR_SUCCESS}YES${RES}"
 else
-    W_ICON="${ICON_KO}"
-    W_TEXT="${R}NO${RES}"
+    W_CHECK="${ICON_KO}"
+    W_TEXT="${COLOR_ERROR}NO${RES}"
 fi
 
 if [[ -x ${FILE} ]]; then
-    X_ICON="${ICON_OK}"
-    X_TEXT="${G}YES${RES}"
+    X_CHECK="${ICON_OK}"
+    X_TEXT="${COLOR_SUCCESS}YES${RES}"
 else
-    X_ICON="${ICON_KO}"
-    X_TEXT="${R}NO${RES}"
+    X_CHECK="${ICON_KO}"
+    X_TEXT="${COLOR_ERROR}NO${RES}"
 fi
 
 # UI
 
-echo -e "${DIM}========================================${RES}"
-echo -e "${BOLD}       FILE INSPECTOR v0.2.0           ${RES}"
-echo -e "${DIM}========================================${RES}"
-echo ""
+echo -e "${COLOR_ACCENT}"
+cat << "EOF"
+===================================================================
+    _______ __        ____                           __
+   / ____(_) /__     /  _/___  _________  ___  _____/ /_____  _____
+  / /_  / / / _ \    / // __ \/ ___/ __ \/ _ \/ ___/ __/ __ \/ ___/
+ / __/ / / /  __/  _/ // / / (__  ) /_/ /  __/ /__/ /_/ /_/ / /
+/_/   /_/_/\___/  /___/_/ /_/____/ .___/\___/\___/\__/\____/_/
+                                /_/
+===================================================================
+EOF
+echo -e "${RES}"
 
-echo -e "${BOLD}TARGET INFO${RES}"
+echo -e "${COLOR_BRAND}${BOLD}TARGET INFO${RES}"
 echo -e "${DIM}-----------${RES}"
-printf "File Name : ${DIM}%s${RES}\n" "${FILE}"
+printf "${BOLD}%-12s${RES} : ${DIM}%b${RES}\n" "File Name" "${W}${FILE}${RES}"
 echo ""
 
-echo -e "${BOLD}PERMISSIONS${RES}"
+echo -e "${COLOR_BRAND}${BOLD}PERMISSIONS${RES}"
 echo -e "${DIM}-----------${RES}"
 
 printf "${BOLD}%-12b %-5b %b${RES}\n" "Type" "Check" "Status"
 echo -e "${DIM}------------ ----- ------${RES}"
 
-printf "${BOLD}%-12b${RES} %-5b %b\n" "Read"    "${R_ICON}" "${R_TEXT}"
-printf "${BOLD}%-12b${RES} %-5b %b\n" "Write"   "${W_ICON}" "${W_TEXT}"
-printf "${BOLD}%-12b${RES} %-5b %b\n" "Execute" "${X_ICON}" "${X_TEXT}"
+printf "${BOLD}%-12b${RES} %b   %b\n" "Read"    "${R_CHECK}" "${R_TEXT}"
+printf "${BOLD}%-12b${RES} %b   %b\n" "Write"   "${W_CHECK}" "${W_TEXT}"
+printf "${BOLD}%-12b${RES} %b   %b\n" "Execute" "${X_CHECK}" "${X_TEXT}"
 
 echo ""
 echo -e "${DIM}========================================${RES}"
